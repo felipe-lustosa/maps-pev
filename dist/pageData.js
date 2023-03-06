edb8cce3c7335463ca9d18d25bd2da2b5da93614
Felipe Lustosa <felipelustosaa6@gmail.com>
2023-03-06 20:41:21 -0300

    build project

---
+++ dist/pageData.js
@@ -0,0 +1,59 @@
+export const pageData = {
+  whatIsPageText: [
+    {
+      title: "",
+      paragraph: ["O CO2 (Dióxido de Carbono) é um composto químico formado por dois átomos de oxigênio e um de carbono. É um produto derivado da reação de diferentes processos, tais como: a combustão do carvão e dos hidrocarbonetos, a fermentação dos líquidos e a respiração dos seres humanos e dos animais, e sua presença, em alta concentração na atmosfera, pode desequilibrar o chamado efeito estufa."]
+    },
+    {
+      title: "Excesso de dióxido de carbono na atmosfera:",
+      paragraph: ["A atividade agropecuária e o transporte são importantes fontes de emissão de CO2 na atmosfera. Além disso, as mudanças no uso da terra (desmatamentos e queimadas) afetam os reservatórios naturais de carbono e, simultaneamente, os sumidouros (ecossistemas com a capacidade de absorver CO2) e os sequestradores de carbono."]
+    },
+    {
+      title: "Qual é o perigo do CO2?",
+      paragraph: ["A alta concentração de dióxido de carbono leva à poluição do ar, formação de chuva ácida e desequilíbrio do efeito estufa, com consequente elevação da temperatura da Terra, que traz consigo os efeitos das mudanças climáticas. Dentre estes efeitos estão o derretimento de calotas de gelo e a elevação dos níveis oceânicos, resultando em uma grande degradação ambiental de ecossistemas e paisagens."]
+    },
+  ],
+  objectivesPageText: [
+    {
+      title: "",
+      paragraph: [
+        "Apresentar de forma sistemática a distribuição da emissão do Dióxido de Carbono por estado brasileiro",
+        "Servir como fonte de pesquisa para estudos da sociedade, acadêmicos e escolares"
+      ]
+    },
+  ],
+  aboutPageText: [
+    {
+      title: "",
+      paragraph: [
+        "O Mapa da Emissão de Carbono no Brasil é uma ferramenta desenvolvida no âmbito do Núcleo Temático de Educação Ambiental Interdisciplinar do Projeto Escola Verde. O objetivo principal é apresentar de forma sistemática os níveis anuais de emissão de CO2 por unidade da federação do Brasil. Os conteúdos inseridos no site são atualizadas automaticamente através de fontes confiáveis, utilizando dados fornecidos pelo SEEG."
+      ]
+    },
+  ],
+  catalogSheetText: [
+    {
+      title: "",
+      paragraph: [
+        "Título: Mapa de Emissões de CO2",
+        "Website: https://pemd.univasf.edu.br/ensinoverde/mapa-emissoes-co2",
+        "Autores: Felipe Lustosa Cavalcanti de Andrade e Prof. Paulo Roberto Ramos (Orientador)",
+        "PEV - Programa Escola Verde. Núcleo Temático de Educação Ambiental Interdisciplinar.",
+        "Juazeiro (BA), Fevereiro de 2023.",
+        "ISBN: 978-65-00-63620-8",
+        "Assuntos: Ferramenta para mostrar a quantidade de emissões de CO2 do Brasil e seus Estados por ano.",
+        "Telas: Mapa de emissões, o que é, objetivos, sobre e fontes",
+      ]
+    },
+  ],
+  referencePageText: [
+    {
+      title: "",
+      paragraph: [
+        "Banco de dados - SEEG - Sistema de Estimativa de Emissões e Remoções de Gases de Efeito Estufa, Observatório do Clima, acessado em [dada] - http://seeg.eco.br",
+        "CO2: Definição e impactos do dióxido de carbono - eCycle. Disponível em: https://www.ecycle.com.br/co2/. Acesso: 29 Jan 2023.",
+        "Dióxido de Carbono: O que é CO2? Disponível em: https://alimentos.com.br/dioxido-de-carbono-o-que-e-co2/. Acesso: 31 Jan 2023.",
+        "Projeto Escola Verde. Disponível em: https://escolaverde.org/site/. Acesso: 16 Fev 2023.",
+      ]
+    },
+  ]
+}

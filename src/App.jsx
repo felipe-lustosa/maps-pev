bd82cbed61636eeade1e37cbc00acfeac7c105de
Felipe Lustosa <felipelustosaa6@gmail.com>
2023-03-06 13:38:11 -0300

    isbn change

--- src/App.jsx
+++ src/App.jsx
@@ -155,7 +155,10 @@ function App() {
         </div>
         <div className="flex flex-col items-center bg-white shadow w-32 md:w-48 lg:w-64">
           <div className="w-full bg-gray-700 px-4 py-2 text-center">
-            <h2 className="font-semibold text-white">ISBN: 978-65-00-63620-8</h2>
+            <h2 className="font-semibold text-white">ISBN</h2>
+          </div>
+          <div className="py-2">
+            <h3 className="font-semibold text-center">978-65-00-63620-8</h3>
           </div>
         </div>
       </div>
